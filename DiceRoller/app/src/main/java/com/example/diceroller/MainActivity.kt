970b6fb6bc1617b2bc03e97b5253deafef2f165b
gurrium <gurrium@hatena.ne.jp>
2021-11-12 19:17:11 +0900

    day 147

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +14,22 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
     }
 
-    private fun rollDice() {
-        val dice = Dice(6)
-        val rollResult = dice.roll()
-        val luckyNumber = 4
-        val rollResultTextView = findViewById<TextView>(R.id.rollResultTextView)
-        val resultMessageTextView = findViewById<TextView>(R.id.resultMessageTextView)
+private fun rollDice() {
+    val dice = Dice(6)
+    val diceImage: ImageView = findViewById(R.id.imageView)
+    val diceRoll = dice.roll()
 
-        rollResultTextView.text = rollResult.toString()
-        when (rollResult) {
-            luckyNumber -> resultMessageTextView.text = "You win!"
-            else -> resultMessageTextView.text = "You lose..."
-        }
+    val resource = when (diceRoll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
+    diceImage.setImageResource(resource)
+    diceImage.contentDescription = diceRoll.toString()
+}
 }
 
 class Dice(private val numSides: Int) {
