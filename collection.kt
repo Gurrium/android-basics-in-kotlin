c6293a289fc3e112d5abcf28cac82aa15bbaac4a
gurrium <gurrium@hatena.ne.jp>
2021-12-30 23:02:41 +0900

    day 194

--- collection.kt
+++ collection.kt
@@ -1,12 +1,10 @@
 fun main() {
-    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
-    println("list:   ${numbers}")
-    println("sorted: ${numbers.sorted()}")
-    val setOfNumbers = numbers.toSet()
-    println("set:    ${setOfNumbers}")
-    val set1 = setOf(1,2,3)
-    val set2 = mutableSetOf(3,2,1)
-    println("$set1 == $set2: ${set1 == set2}")
-	println("contains 7: ${setOfNumbers.contains(7)}")
+    val peopleAges = mutableMapOf<String, Int>(
+        "Fred" to 30,
+        "Ann" to 23
+    )
+    peopleAges.put("Barbara", 42)
+    peopleAges["Joe"] = 51
+    peopleAges["Fred"] = 31
+    println(peopleAges)
 }
-
