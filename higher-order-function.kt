611895b18afab3a1c4a5bb6e076f6cb1c67f59e1
gurrium <gurrium@hatena.ne.jp>
2022-01-03 21:41:31 +0900

    day 198

---
+++ higher-order-function.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val peopleNames = listOf("Fred", "Ann", "Barbara", "Joe")
+    println(peopleNames.sorted())
+	println(peopleNames.sortedWith { str1: String, str2: String -> str1.length - str2.length })
+}
