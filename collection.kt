796867c5bfa00daa72fde2075d311cdecc9e5701
gurrium <gurrium@hatena.ne.jp>
2022-01-05 19:57:44 +0900

    day 200

--- collection.kt
+++ collection.kt
@@ -1,14 +1,12 @@
 fun main() {
-    val peopleAges = mutableMapOf<String, Int>(
-        "Fred" to 30,
-        "Ann" to 23
-    )
-    peopleAges.put("Barbara", 42)
-    peopleAges["Joe"] = 51
-    peopleAges["Fred"] = 31
-    peopleAges.forEach { print("${it.key} is ${it.value}, ") }
-    println()
-    println(peopleAges.map { "${it.key} is ${it.value}" }.joinToString(", ") )
-    val filteredNames = peopleAges.filter { it.key.length < 4 }
-    println(filteredNames)
+    val words = listOf("about", "acute", "awesome", "balloon", "best", "brief", "class", "coffee", "creative")
+    val filteredWordsStartWithB = words.filter { it.startsWith("b", ignoreCase = true) }
+    .shuffled()
+    .take(2)
+    .sorted()
+	println(filteredWordsStartWithB)
+    val filteredWordsStartWithC = words.filter { it.startsWith("c", ignoreCase = true) }
+    .shuffled()
+    .take(1)
+	println(filteredWordsStartWithC)
 }
